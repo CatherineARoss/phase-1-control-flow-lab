50aea40759de8b691e71fe673778173a43b964f0
Catherine Ross <fibervibes@gmail.com>
2023-03-14 22:35:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride === 199) { 
+    return ('This one is on me!');
+  } 
+  else if (ride === 1500) {
+    return ('That will be twenty bucks.');
+  }
+  else if (ride === 2001 ) {
+    return ('I will gladly take your thirty bucks.');
+  }
+  else if (ride === 2501) {
+    return ('No can do.');
+  }
+}
+ 
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
